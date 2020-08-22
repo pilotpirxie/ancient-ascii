986b7de9190656a529c78afae8eb89266b023f85
loleksatyr <czarnotabartek@gmail.com>
2020-08-23 01:40:20 +0200

    ShopUpdate

--- game/src/com/testgame/Main.java
+++ game/src/com/testgame/Main.java
@@ -2,6 +2,8 @@ package com.testgame;
 
 import java.util.Scanner;
 
+import static java.lang.Integer.parseInt;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -11,7 +13,7 @@ public class Main {
         System.out.println("\u001b[33m");
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Choose mode: ");
+        //System.out.println("Choose mode: ");
 
         System.out.println("First player nickname:");
         while (!scanner.hasNextLine()) {
@@ -33,13 +35,30 @@ public class Main {
         while (board.isEconomyState()) {
             menu.displayStats();
             board.drawBoard();
-            shop.displayShopMenu();
-            BuildingType order = shop.readShop(board.getCurrentPlayer());
-            if (order != null) {
-                board.addBuilding(order, board.getCurrentPlayer());
-            } else{
-                System.out.println("Nothing bought...");
+            int menudis;
+            System.out.println("==".repeat(20));
+            System.out.println("Press 0 - to skip turn | Press 1 - to open the shop");
+            System.out.println("==".repeat(20));
+            while (!scanner.hasNextLine()) {
+                scanner.next();
             }
+            menudis = parseInt(scanner.nextLine());
+            if (menudis == 1){
+
+                shop.displayShopMenu();
+                BuildingType order = shop.readShop(board.getCurrentPlayer());
+                if (order != null) {
+                    board.addBuilding(order, board.getCurrentPlayer());
+                } else{
+                    System.out.println("Nothing bought...");
+                }
+            }
+            else {
+
+
+            }
+
+
             board.togglePlayer();
         }
 
--- game/src/com/testgame/Shop.java
+++ game/src/com/testgame/Shop.java
@@ -5,15 +5,99 @@ import java.util.Scanner;
 public class Shop {
     public void displayShopMenu() {
         System.out.println("==".repeat(20));
-        System.out.println("0 Nothing, wait...");
-        System.out.println("1 C Castle | +5 Gold/turn | -30 GOLD");
-        System.out.println("2 V Village | +1 Gold/turn | -8 GOLD");
-        System.out.println("3 P Pyramid | +5 Morale | -15 GOLD");
-        System.out.println("4 S Temple | +1 Morale | -5 GOLD");
-        System.out.println("5 A Armory | +5 Army | -15 GOLD");
-        System.out.println("6 F Farm | +1 Army | -4 GOLD");
-        System.out.println("7 T Archer Tower | +5 Defence | -10 GOLD");
-        System.out.println("8 W Wall | +1 Defence | -4 GOLD");
+        System.out.println("0 - Skip turn");
+        System.out.println("1 - PALACE - 30 GOLD");
+        System.out.println( "  / \\      ^        / \\       \n" +
+                " /   \\             /   \\      \n" +
+                "         (_____)           \n" +
+                " |   |  _   _   _  |   |    \n" +
+                "                \n" +
+                " |   |   - _^_     |   |    \n" +
+                " |  _|    //|\\\\  - |   |     \n" +
+                " |   |   ///|\\\\\\   |  -|      \n" +
+                " |-  |_  |||||||   |   |      \n" +
+                " |   |   |||||||   |-  | ");
+        System.out.println("        +5 Gold/turn ");
+        System.out.println("==".repeat(20));
+        System.out.println("2 - VILLAGE - 8 GOLD");
+        System.out.println("~         ~~          __\n" +
+                "       _T      .,,.    ~--~ ^^\n" +
+                " ^^   // \\                    ~\n" +
+                "      ][O]    ^^      ,-~ ~\n" +
+                "   /''-I_I         _II____\n" +
+                "__/_  /   \\ ______/ ''   /'\\_,__\n" +
+                "  | II--'''' \\,--:--..,_/,.-{ },\n" +
+                "; '/__\\,.--';|   |[] .-.| O{ _ }\n" +
+                ":' |  | []  -|   ''--:.;[,.'\\,/\n" +
+                "'  |[]|,.--'' '',   ''-,.    |\n" +
+                "  ..    ..-''    ;       ''. '");
+        System.out.println("        +1 Gold/turn ");
+        System.out.println("==".repeat(20));
+        System.out.println("3 - PYRAMID - 15 GOLD");
+        System.out.println("          ,/`.\n" +
+                "        ,'/ __`.\n" +
+                "      ,'_/_  _ _`.\n" +
+                "    ,'__/_ ___ _  `.\n" +
+                "  ,'_  /___ __ _ __ `.\n" +
+                " '-.._/___...-\"-.-..__`. ");
+        System.out.println("        +5 Morale/turn ");
+        System.out.println("==".repeat(20));
+        System.out.println("4 TEMPLE - 5 GOLD");
+        System.out.println("                  /\\\n" +
+                "     /\\          (  )  /\\\n" +
+                "    (  )         |  | /  \\\n" +
+                "    |  |         |   (    )\n" +
+                "    |  |_________|   |    |\n" +
+                "    |                |    |");
+        System.out.println("        +1 Morale/turn ");
+        System.out.println("==".repeat(20));
+        System.out.println("5 ARMORY - 15 GOLD");
+        System.out.println(" _   |~  _\n" +
+                "[_]--'--[_]\n" +
+                "|'|\"\"`\"\"|'|\n" +
+                "| | /^\\ | |\n" +
+                "|_|_|I|_|_|");
+        System.out.println("        +5 Army/turn ");
+        System.out.println("==".repeat(20));
+        System.out.println("6 FARM - 4 GOLD");
+
+        System.out.println("                           _.-^-._    .--.\n" +
+                "                        .-'   _   '-. |__|\n" +
+                "                       /     |_|     \\|  |\n" +
+                "                      /               \\  |\n" +
+                "                     /|     _____     |\\ |\n" +
+                "                      |    |==|==|    |  |\n" +
+                "  |---|---|---|---|---|    |--|--|    |  |\n" +
+                "  |---|---|---|---|---|    |==|==|    |  |");
+        System.out.println("        +1 Army/turn ");
+        System.out.println("==".repeat(20));
+        System.out.println("7 ARCHER TOWER - 10 GOLD");
+        System.out.println("                    .. - ----.\n" +
+                "                     I'__. ..- '';\n" +
+                "                     |           .\n" +
+                "                     J.\n" +
+                "                      L:..._.- -' .\n" +
+                "                      |.\n" +
+                "                      J:.\n" +
+                "                       L:.. ..- -' .\n" +
+                "                       |.\n" +
+                "                       J:.\n" +
+                "                        LP:._..- -' .\n" +
+                "                        |:.\n" +
+                "       ________________ J8:,          ______________a:f____" +
+                "");
+        System.out.println("        +5 Defence/turn ");
+        System.out.println("==".repeat(20));
+        System.out.println("8 WALL - 4 GOLD");
+        System.out.println("_|___|___|___|___|___|___|___|___|___|___|___|___|___|\n" +
+                "___|___|___|___|___|___|___|___|___|___|___|___|___|__\n" +
+                "_|___|___|___|___|___|___|___|___|___|___|___|___|___|\n" +
+                "___|___|___|___|___|___|___|___|___|___|___|___|___|__\n" +
+                "_|___|___|___|___|___|___|___|___|___|___|___|___|___|\n" +
+                "___|___|___|___|___|___|___|___|___|___|___|___|___|__\n" );
+
+        System.out.println("        +1 Defence/turn ");
+        System.out.println("==".repeat(20));
         System.out.println("Pick option 0-8: ");
     }
 
@@ -50,7 +134,7 @@ public class Shop {
             case 1:
                 if (buy(player, 30)) {
                     player.addGoldIncrease(5);
-                    return BuildingType.CASTLE;
+                    return BuildingType.PALACE;
                 }
                 return null;
             case 2:
